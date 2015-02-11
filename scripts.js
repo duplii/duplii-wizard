81a626310873308be130e5fa0f88f16520f595d4
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2015-02-11 13:44:36 +0100

    Move scripts to a js file
    Scripts now reside on their own .js file.
    
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

---
+++ scripts.js
@@ -0,0 +1,42 @@
+function animateSmile() {
+	// Great reference and due credit: http://jakearchibald.com/2013/animated-line-drawing-svg/
+	var svgDocument = document.getElementById('blinking-face-obj').contentDocument;
+	var paths = svgDocument.querySelectorAll('#duplii-logo-face path');
+	var delay = 0.8;
+	for (var i = 0; i < paths.length; i++) {
+		var path = paths[i];
+		var length = path.getTotalLength();
+		// Clear any previous transition
+		path.style.transition = path.style.WebkitTransition =
+		  'none';
+		// Set up the starting positions
+		path.style.strokeDasharray = length + ' ' + length;
+		path.style.strokeDashoffset = length;
+		path.style.stroke = '#FFFFFF';
+		path.style.fill = 'transparent';
+		// Trigger a layout so styles are calculated & the browser
+		// picks up the starting position before animating
+		path.getBoundingClientRect();
+		delay = delay + 0.1;
+		// Define our transition
+		path.style.transition = path.style.WebkitTransition =
+		  'stroke-dashoffset .7s ease-in-out '+ delay +'s, fill .2s ease-in 1.7s';
+		// Go!
+		path.style.strokeDashoffset = '0';
+		path.style.fill = '#FFFFFF';
+	}
+}
+
+function animateSvgSmil(objId, animation) {
+	var svgDocument = document.getElementById(objId).contentDocument;
+	svgDocument.getElementById(animation).beginElement();
+}
+
+window.addEventListener('load', function() {
+	document.getElementById('magic-dot').addEventListener('click', function() {
+		this.classList.add('magic-dot-expanded');
+		document.getElementById('magic-text-container').classList.add('magic-text-container-appear');
+		animateSmile();
+		setTimeout(function(){animateSvgSmil('blinking-face-obj', 'blink');}, 2500);
+	});
+});
