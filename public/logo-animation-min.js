bfb529d779cdb777a970f440731ee4d6e155f342
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2015-02-13 17:49:15 +0100

    Stop fucking minifying scripts
    You heard it.
    
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- public/logo-animation-min.js
+++ public/logo-animation-min.js
@@ -1,1 +1,37 @@
-function animateSmile(){for(var e=document.getElementById("blinking-face-obj").contentDocument,t=e.querySelectorAll("#duplii-logo-face path"),n=.4,s=0;s<t.length;s++){var l=t[s],o=l.getTotalLength();l.style.transition=l.style.WebkitTransition="none",l.style.strokeDasharray=o+" "+o,l.style.strokeDashoffset=o,l.style.stroke="#FFFFFF",l.style.fill="transparent",l.getBoundingClientRect(),n+=.1,l.style.transition=l.style.WebkitTransition="stroke-dashoffset .7s ease-in-out "+n+"s, fill .2s ease-in 1.3s",l.style.strokeDashoffset="0",l.style.fill="#FFFFFF"}}function animateSvgSmil(e,t){var n=document.getElementById(e).contentDocument;n.getElementById(t).beginElement()}
+/***************** Smile animations *****************/
+
+function animateSmile() {
+	// Great reference and due credit: http://jakearchibald.com/2013/animated-line-drawing-svg/
+	var svgDocument = document.getElementById('blinking-face-obj').contentDocument;
+	var paths = svgDocument.querySelectorAll('#duplii-logo-face path');
+	var delay = 0.4;
+	for (var i = 0; i < paths.length; i++) {
+		var path = paths[i];
+		var length = path.getTotalLength();
+		// Clear any previous transition
+		path.style.transition = path.style.WebkitTransition =
+		  'none';
+		// Set up the starting positions
+		path.style.strokeDasharray = length + ' ' + length;
+		path.style.strokeDashoffset = length;
+		path.style.stroke = '#FFFFFF';
+		path.style.fill = 'transparent';
+		// Trigger a layout so styles are calculated & the browser
+		// picks up the starting position before animating
+		path.getBoundingClientRect();
+		delay = delay + 0.1;
+		// Define our transition
+		path.style.transition = path.style.WebkitTransition =
+		  'stroke-dashoffset .7s ease-in-out '+ delay +'s, fill .2s ease-in 1.3s';
+		// Go!
+		path.style.strokeDashoffset = '0';
+		path.style.fill = '#FFFFFF';
+	}
+}
+
+function animateSvgSmil(objId, animation) {
+	var svgDocument = document.getElementById(objId).contentDocument;
+	svgDocument.getElementById(animation).beginElement();
+}
+
+
--- public/scripts-min.js
+++ public/scripts-min.js
@@ -1,1 +1,19 @@
-window.addEventListener("load",function(){var e=document.querySelector(".bubble-animation");celebrate(e,10),document.getElementById("end-wizard").addEventListener("click",function(){stopCelebration(),document.getElementById("magic-dot").classList.add("magic-dot-expanded"),document.querySelector(".wizard-end-container").classList.add("wizard-end-appear"),document.querySelector(".magic-text-container").classList.add("magic-text-container-appear"),animateSmile(),setTimeout(function(){animateSvgSmil("blinking-face-obj","blink")},1700)})});
+/***************** Fire them all! *****************/
+
+window.addEventListener('load', function() {
+	// Set a place where we want the bubble thing to fire.
+	var place = document.querySelector('.bubble-animation');
+	
+	// Fire the thing!	
+	celebrate(place, 10);
+	
+	document.getElementById('end-wizard').addEventListener('click', function() {
+		stopCelebration();
+		document.getElementById('magic-dot').classList.add('magic-dot-expanded');
+		document.querySelector('.wizard-end-container').classList.add('wizard-end-appear');
+		document.querySelector('.magic-text-container').classList.add('magic-text-container-appear');
+		animateSmile();
+		setTimeout(function(){animateSvgSmil('blinking-face-obj', 'blink');}, 1700);
+	});
+});
+
