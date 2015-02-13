b14b6a320c7eea4d9b55e3e787c59864ed0ed013
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2015-02-13 13:08:11 +0100

    Include Bubble anim and use discrete files
    Include the bubble animation stuff and move everything to own files, so each animation has two sets of files: one for styles and one for js. These are now included singularly in the html. This is in order to avoid confusion in the developers, so that the final file structure is up to them and their current flow.
    
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

---
+++ bubble-animation.js
@@ -0,0 +1,63 @@
+/***************** Bubble animations *****************/
+
+function makeBubble( where ){
+	// Helper function to get random numbers.
+	function getRandomInt(min, max) {
+	    return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+	// Define all variables.
+	var newBubble, diameter, alphaChannel, color, cssString, animationId, posLeft, posBottom, animDelay;
+	// Define the diameter of the bubble.
+	diameter = getRandomInt(10, 25);
+	// Define the color. First we get the alpha channel and then create the rgba value. Rgba is used instead of solid colors so that we can create a nice overlapping effect without touching opacity. Opacity is in fact used in the animation for bursting the bubble.
+	alphaChannel = getRandomInt(3, 8) / 10;
+	color = '255, 130, 76, ' + alphaChannel;
+	// Define the position bottom and left.
+	posLeft = getRandomInt(0, 100);
+	posBottom = getRandomInt(-25, 50);
+	// Define an optional animation delay.
+	animDelay = getRandomInt(0, 3) * 100;
+	// Create the css string to be added to the element.
+	cssString = 'width:'+ diameter +'px;' +
+		'height:'+ diameter +'px;' +
+		'bottom:'+ posBottom +'%;' +
+		'left:'+ posLeft +'%;' +
+		'background-color:rgba('+ color +');' +
+		'-webkit-animation-delay:'+ animDelay +'ms;' +
+		'animation-delay:'+ animDelay +'ms;';
+	// Decide what animation to apply.
+	animationId = getRandomInt(1,3);
+	// Create the thing.
+	newBubble = document.createElement('span');
+	// Apply classes and styles.
+	newBubble.classList.add('bubble', 'bubble-burst-'+ animationId);
+	newBubble.style.cssText +=';'+ cssString;
+	
+	// Append it where you're told to.
+	where.appendChild(newBubble);
+	
+	// We need to calculate how much time will the animation last (delay + duration). The duration needs to be in milliseconds, hence the * 1000.
+	function getAnimationDuration( el ) {
+		var elStyle = getComputedStyle(el);
+		var animDuration = elStyle.getPropertyValue('animation-duration') || 
+				elStyle.getPropertyValue('-webkit-animation-duration');
+			animDuration = parseFloat(animDuration) * 1000;
+			animDelay = elStyle.getPropertyValue('animation-delay') || 
+					elStyle.getPropertyValue('-webkit-animation-delay');
+				animDelay = parseFloat(animDelay) * 1000;
+		return animDuration + animDelay;
+	}
+	
+	// We then wait for the animation to end, and then we remove the element and fire the main function all over again.
+	setTimeout(function () {
+		newBubble.parentNode.removeChild(newBubble);
+		makeBubble(where);
+	}, getAnimationDuration(newBubble));
+}
+
+// This is the function that will actually fire the bubbles. It accepts a DOMNode where the bubbles should appear and the quantity of bubbles to create.
+function celebrate(place, quantity) {
+	for (var i = 0; i < quantity; i++) {
+		makeBubble(place);
+	} 
+}
--- scripts.js
+++ scripts.js
@@ -1,38 +1,12 @@
-function animateSmile() {
-	// Great reference and due credit: http://jakearchibald.com/2013/animated-line-drawing-svg/
-	var svgDocument = document.getElementById('blinking-face-obj').contentDocument;
-	var paths = svgDocument.querySelectorAll('#duplii-logo-face path');
-	var delay = 0.8;
-	for (var i = 0; i < paths.length; i++) {
-		var path = paths[i];
-		var length = path.getTotalLength();
-		// Clear any previous transition
-		path.style.transition = path.style.WebkitTransition =
-		  'none';
-		// Set up the starting positions
-		path.style.strokeDasharray = length + ' ' + length;
-		path.style.strokeDashoffset = length;
-		path.style.stroke = '#FFFFFF';
-		path.style.fill = 'transparent';
-		// Trigger a layout so styles are calculated & the browser
-		// picks up the starting position before animating
-		path.getBoundingClientRect();
-		delay = delay + 0.1;
-		// Define our transition
-		path.style.transition = path.style.WebkitTransition =
-		  'stroke-dashoffset .7s ease-in-out '+ delay +'s, fill .2s ease-in 1.7s';
-		// Go!
-		path.style.strokeDashoffset = '0';
-		path.style.fill = '#FFFFFF';
-	}
-}
-
-function animateSvgSmil(objId, animation) {
-	var svgDocument = document.getElementById(objId).contentDocument;
-	svgDocument.getElementById(animation).beginElement();
-}
+/***************** Fire them all! *****************/
 
 window.addEventListener('load', function() {
+	// Set a place where we want the bubble thing to fire.
+	var place = document.querySelector('.bubble-animation');
+	
+	// Fire the thing!	
+	celebrate(place, 10);
+	
 	document.getElementById('magic-dot').addEventListener('click', function() {
 		this.classList.add('magic-dot-expanded');
 		document.querySelector('.magic-text-container').classList.add('magic-text-container-appear');
