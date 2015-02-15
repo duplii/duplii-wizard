edbf058d79b5819f82a82c00afa2d2b63d6a6c8e
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2015-02-15 15:05:09 +0100

    Bringing it all together
    Bringing it all together: layout, transitions, animations, logic. Works fine. We can start thinking about publishing.
    
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- public/scripts-min.js
+++ public/scripts-min.js
@@ -5,8 +5,13 @@ window.addEventListener('load', function() {
 	// Start the celebration! Woooo!	
 	celebrate();
 	
-	document.getElementById('end-wizard').addEventListener('click', function() {
+	document.querySelector('.modal-button--show-next').addEventListener('click', function() {
 		stopCelebration();
+		document.querySelector('.modal-page--first').classList.add('modal-page--hidden');
+		document.querySelector('.modal-page--last').classList.remove('modal-page--hidden');
+	});
+	
+	document.getElementById('end-wizard').addEventListener('click', function() {
 		document.getElementById('magic-dot').classList.add('magic-dot-expanded');
 		document.querySelector('.wizard-end-container').classList.add('wizard-end-appear');
 		document.querySelector('.magic-text-container').classList.add('magic-text-container-appear');
